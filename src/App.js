12f64139b1d7c2642a821c2e85aa1444f662369b
Akshay Kedlaya <akshaykedlaya@divum.in>
2023-07-18 14:32:26 +0530

    test for youtube video default events

--- src/App.js
+++ src/App.js
@@ -5,25 +5,26 @@ import { useEffect } from 'react';
 
 function App() {
     useEffect(() => {
-      ReactGA.initialize("UA-275881285-1");
+      ReactGA.initialize("G-ERGQGM3LY6");
     },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>still on testing</p>
-      </header>
+
+{/* <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name:</label>
+      <input type="text" id="name" name="name" value={formData.name} onChange={handleChange}/>
+
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" name="email" value={formData.email} onChange={handleChange}/>
+
+      <label htmlFor="message">Message:</label>
+      <textarea id="message" name="message" value={formData.message} onChange={handleChange}/>
+
+      <button type="submit">Submit</button>
+    </form> */}
+
+      <iframe width="1280" height="720" src="https://www.youtube.com/embed/SqcY0GlETPk" title="React Tutorial for Beginners" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
     </div>
   );
 }
